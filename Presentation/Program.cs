b83075b5d4d03e4608eadadcaf51eb35219e86ea
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2024-10-02 12:05:09 +0100

    added README.md file

---
+++ Presentation/Program.cs
@@ -0,0 +1,29 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationService()
+    .AddInfrastructureService(builder.Configuration)
+    .AddPresentationService();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
